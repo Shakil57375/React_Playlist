9ed0ef05f12712b92a5419b79feaaa96473331cc
Shakil57375 <shakil57375@gmail.com>
2023-11-15 15:27:59 +0600

    class Component and functional component added

--- 6/my-app-2/src/main.jsx
+++ 6/my-app-2/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Clock from './ClassComponet.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App locale = "bn-BD" />
+    <Clock locale = "bn-BD" />
   </React.StrictMode>,
 )
