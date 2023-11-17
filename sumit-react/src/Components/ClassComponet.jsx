b9b18395ad180ebfba18d7895b50dc0481446db5
Shakil57375 <shakil57375@gmail.com>
2023-11-17 19:17:59 +0600

    what will be this when using normal and arrow function

--- sumit-react/src/Components/ClassComponet.jsx
+++ sumit-react/src/Components/ClassComponet.jsx
@@ -4,7 +4,7 @@ import React from "react";
 class Clock extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { date: new Date() };
+    this.state = { date: new Date(), locale: "bn-BD" };
   }
 
   componentDidMount() {
@@ -23,12 +23,21 @@ class Clock extends React.Component {
     });
   }
 
+  handleClick(e) {
+    e.preventDefault();
+    this.setState({
+      locale: "en-US",
+    });
+  }
+
   render() {
+    const { date, locale } = this.state;
     return (
       <h1 className="heading">
-        <span className="text">
-          {this.state.date.toLocaleTimeString(this.props.locale)}
-        </span>
+        <span className="text">{date.toLocaleTimeString(locale)}</span>
+        <button type="submit" onClick={this.handleClick}>
+          Click me
+        </button>
       </h1>
     );
   }
