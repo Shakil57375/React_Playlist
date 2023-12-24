2f8fec7937d104a557b26283a98eaeeae2e90ffe
Shakil57375 <shakil57375@gmail.com>
2023-12-24 14:25:36 +0600

    react state management

--- react-state-mangement/src/App.jsx
+++ react-state-mangement/src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from "react";
 import "./App.css";
-function App() {
-  let [count, setCount] = useState(0);
+function Counter({ value }) {
+  let initialValue = value;
+  let [count, setCount] = useState(initialValue);
   const IncreaseCount = () => {
-    setCount(count + 1)
+    setCount(count + 1);
   };
   return (
     <div className="card">
       <button onClick={IncreaseCount}>count is {count}</button>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="card">
+      <Counter value={3} />
+      <Counter value={0} />
     </div>
   );
 }
